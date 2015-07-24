8d448a3c925b2bc0a8e9f930cc6985ddd8a9663d
George Welsted <george@gwelsted.com>
2015-07-24 12:36:33 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Linkbase::Application.config.secret_key_base = 'a701e307ed190a26149fd7ef17fec72d966ecd725a3f409adf55212c66fd023f4878c6e75d985648b914fd305c2cb14a43bdc6d9355d8ecbcc2ec7880dd407e5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Linkbase::Application.config.session_store :cookie_store, key: '_linkbase_session'
