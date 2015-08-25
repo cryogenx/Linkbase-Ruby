c1399df6bc6875cd2c4539bc5f66ffee133d965a
George Welsted <george@gwelsted.com>
2015-08-25 01:56:12 +0000

    Cleaned up Dashboard and Shared UI, re-routed linkbases to not have to use the "show" in the URL for public sites, fixed some responsivness issues

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Linkbase::Application.routes.draw do
   devise_for :users
   resource :dashboard, :only => [:show]
+  get "linkbases" => "linkbases#show"
   resources :linkbases, :only => [:show]
+
   
   root 'static_pages#index'
 
