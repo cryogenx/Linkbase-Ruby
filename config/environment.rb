d0891109b3e8c25271d395642ff1b83829a02dda
George Welsted <george@gwelsted.com>
2015-08-25 23:12:54 +0000

    adding logger

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,5 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Linkbase::Application.initialize!
+
+Rails.logger = Le.new('bc2a4c66-4e42-4715-a14e-2656b97d5a6e', :debug => true, :local => true)
