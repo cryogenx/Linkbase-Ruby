d90e61e2ab8610638607ec98f53ef374fde1c726
George Welsted <george@gwelsted.com>
2015-09-10 14:20:38 +0000

    fixing a bug with the sharekey

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,14 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  has_many :links
-  has_many :categories
+  has_many :links, dependent: :destroy
+  has_many :categories, dependent: :destroy
   
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates :share_key, presence: true
+  validates_uniqueness_of  :email
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Linkbase::Application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { registrations: "users/registrations" }
   resource :dashboard, :only => [:show]
   get "linkbases" => "linkbases#show"
   resources :linkbases, :only => [:show]
